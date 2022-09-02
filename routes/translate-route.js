31fb364cb6be23072acaf949ee2398894a6756bd
Luan Fernandes <luanfern2004@gmail.com>
2022-09-02 16:50:31 -0300

    change controller in other file

--- routes/translate-route.js
+++ routes/translate-route.js
@@ -3,11 +3,6 @@ const lib = require('../lib/lib');
 
 const Router = express.Router();
 
-Router.post('/', (req, res, next) => {
-    let r = req.body['translate'];
-    res.status(200).send({
-        "return": lib['translate'](r),
-    });
-});
+Router.post('/', lib.translate);
 
 module.exports = Router;
