deba5d5292e0d9c0b55204e9defea6725cae7e44
Luan Fernandes <luanfern2004@gmail.com>
2022-09-02 16:50:01 -0300

    add parser of urlencoded

--- app/app.js
+++ app/app.js
@@ -7,6 +7,7 @@ const translateRoute = require('../routes/translate-route');
 const app = express();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', translateRoute);
 
 module.exports = app;
